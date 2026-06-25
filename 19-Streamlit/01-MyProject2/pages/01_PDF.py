6fbec9af963f4301f3bf3ab467d6782bc4115ba7
Zenith Kim <zetta.kim@icloud.com>
2026-06-25 22:22:54 +0900

    6/25-f

--- 19-Streamlit/01-MyProject2/pages/01_PDF.py
+++ 19-Streamlit/01-MyProject2/pages/01_PDF.py
@@ -5,6 +5,14 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_teddynote.prompts import load_prompt
 from langchain_openai import ChatOpenAI
 from langchain import hub
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
 from dotenv import load_dotenv
 import glob
 import os
@@ -20,8 +28,6 @@ if not os.path.exists(".cache/files"):
 if not os.path.exists(".cache/embeddings"):
     os.mkdir(".cache/embeddings")
 
-
-
 # API Key 정보 로드
 load_dotenv()
 
@@ -30,6 +36,10 @@ st.title("PDF 기반 QA💬")
 # 처음 한 번 실행 필요, 이후에는 초기화 안됨
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
+
+if "chain" not in st.session_state:
+    # 아무런 파일을 업로드 하지 않았을 경우
+    st.session_state["chain"] = None
 
 # 사이드바 생성
 with st.sidebar:
@@ -51,7 +61,7 @@ def print_messages():
 def add_message(role, message):
     st.session_state["messages"].append(ChatMessage(role=role, content=message))
 
-# vkdlfdmf 캐시에 저장(시간이 오래 걸리는 작업을 처리할 예정)
+# 업로드 파일 캐시에 저장(시간이 오래 걸리는 작업을 처리할 예정)
 @st.cache_resource(show_spinner="업로드 파일을 처리 중입니다...")
 def embed_file(file):
     # 업로드한 파일을 캐시 디레토리에 저장합니다.
@@ -60,26 +70,70 @@ def embed_file(file):
     with open(file_path, "wb") as f:
         f.write(file_content)
 
-# 파일이 업로드 되었을 때
-if uploaded_file:
-    embed_file(uploaded_file)
+
+    # 단계 1: 문서 로드(Load Documents)
+    loader = PyMuPDFLoader(file_path)
+    docs = loader.load()
+
+    # 단계 2: 문서 분할(Split Documents)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+    split_documents = text_splitter.split_documents(docs)
+
+    # 단계 3: 임베딩(Embedding) 생성
+    embeddings = OpenAIEmbeddings()
+
+    # 단계 4: DB 생성(Create DB) 및 저장
+    # 벡터스토어를 생성합니다.
+    vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
+
+    # 단계 5: 검색기(Retriever) 생성
+    # 문서에 포함되어 있는 정보를 검색하고 생성합니다.
+    retriever = vectorstore.as_retriever()
+    return retriever
 
 # 체인 생성
-def create_chain(prompt_filepath): 
+def create_chain(retriever): 
     # prompt | llm | oitput_parser
 
     # Prompt 적용
-    prompt = load_prompt(prompt_filepath, encoding="utf-8")
+    # prompt = load_prompt(prompt_filepath, encoding="utf-8")
  
-    # llm
-    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+    # 단계 6: 프롬프트 생성(Create Prompt)
+    # 프롬프트를 생성합니다.
+    prompt = PromptTemplate.from_template(
+        """You are an assistant for question-answering tasks. 
+    Use the following pieces of retrieved context to answer the question. 
+    If you don't know the answer, just say that you don't know. 
+    Answer in Korean.
 
-    # 출력 파서
-    output_parser = StrOutputParser()
+    #Context: 
+    {context}
 
-    # 체인 생성
-    chain = prompt | llm | output_parser
+    #Question:
+    {question}
+
+    #Answer:"""
+    )
+
+    # 단계 7: 언어모델(LLM) 생성
+    # 모델(LLM) 을 생성합니다.
+    llm = ChatOpenAI(model_name="gpt-4.1-mini", temperature=0)
+
+    # 단계 8: 체인(Chain) 생성
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
     return chain
+
+# 파일이 업로드 되었을 때
+if uploaded_file:
+    # 파일 업로드 후 retriever 생성 (작업 시간이 오래 걸릴 예정)
+    retriever = embed_file(uploaded_file)
+    chain = create_chain(retriever)
+    st.session_state["chain"] = chain
 
 # 초기화 버튼이 눌리면...
 if clear_btn:
@@ -97,19 +151,21 @@ if user_input:
     st.chat_message("user").write(user_input)
     
     # chain 생성
-    chain = create_chain(selected_prompt)
+    chain = st.session_state["chain"]
 
-    # 스트리밍 호출
-    response = chain.stream({"question": user_input})
-    with st.chat_message("assistant"):
-        # 빈 공간(컨테이너)을 만들어서, 여기에 토근을 스트리밍 출력한다.
-        container = st.empty()
+    if chain is not None:
+        # 스트리밍 호출
+        response = chain.stream({"question": user_input})
+        with st.chat_message("assistant"):
+            # 빈 공간(컨테이너)을 만들어서, 여기에 토근을 스트리밍 출력한다.
+            container = st.empty()
 
-        ai_answer = ""
-        for token in response:
-            ai_answer += token
-            container.markdown(ai_answer)
+            ai_answer = ""
+            for token in response:
+                ai_answer += token
+                container.markdown(ai_answer)
 
-    # 대화기록을 저장한다
-    add_message("user", user_input)
-    add_message("assistant", ai_answer)
+        # 대화기록을 저장한다
+        add_message("user", user_input)
+        add_message("assistant", ai_answer)
+    
